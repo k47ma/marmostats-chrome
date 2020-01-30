fa618123bb4f688df6d65adae2f04b8548bf0670
Kai Ma <k47ma@edu.uwaterloo.ca>
2020-01-30 12:28:40 -0500

    Added max score column in overview table

--- overview.js
+++ overview.js
@@ -24,7 +24,7 @@ function update_total_students(subject, catalog) {
 }
 
 // update the submission rate and correctness rate for each project
-function update_project_stats(project_name, target_tag, total_students, projects) {
+function update_project_stats(project_name, rate_tag, score_tag, total_students, projects) {
     const project_id = projects[project_name]['id'];
     const project_url = "https://marmoset.student.cs.uwaterloo.ca/view/instructor/projectTestResults.jsp?projectPK="+project_id;
 
@@ -60,7 +60,8 @@ function update_project_stats(project_name, target_tag, total_students, projects
 
         const submission_rate = (total_submissions / total_students * 100).toFixed(1);
         const avg_score = (mean / max * 100).toFixed(1);
-        target_tag.innerHTML = '<b>' + submission_rate + '%</b>' + '/' + '<b>' + avg_score + '%</b>';
+        rate_tag.innerHTML = '<b>' + submission_rate + '%</b>' + '/' + '<b>' + avg_score + '%</b>';
+        score_tag.innerText = max;
         projects[project_name]['submission'] = submission_rate;
         projects[project_name]['correctness'] = avg_score;
 
@@ -80,11 +81,10 @@ function draw_chart(projects) {
 
     // draw progress bar if not all projects are done
     var progress_container = document.getElementById('marmostats-progress');
-    var progress_tag = document.getElementById('marmostats-progress-bar-background');
     var bar_tag = document.getElementById('marmostats-progress-bar');
     var perc_tag = document.getElementById('marmostats-progress-perc');
     const percent = (finished_projects / total_projects * 100).toFixed(1) + '%';
-    
+
     progress_container.style.visibility = 'visible';
     bar_tag.style.width = percent;
     perc_tag.innerText = percent;
@@ -194,25 +194,32 @@ function add_test_details(total_students) {
     var project_table = document.getElementsByTagName('table')[0];
     var rows = project_table.getElementsByTagName('tr');
 
-    var title_tag = document.createElement('th');
-    title_tag.innerHTML = "Submission<br />/Avg. Score";
-    rows[0].insertBefore(title_tag, rows[0].children[1]);
+    var title_rate_tag = document.createElement('th');
+    title_rate_tag.innerHTML = "Submission<br />/Avg. Score";
+    rows[0].insertBefore(title_rate_tag, rows[0].children[1]);
+
+    var title_score_tag = document.createElement('th');
+    title_score_tag.innerHTML = "Max<br />Score";
+    rows[0].insertBefore(title_score_tag, rows[0].children[2]);
 
     var projects = new Object();
     for (var i = 1; i < rows.length; ++i) {
         if (rows[i].children[0].hasAttribute('colspan')) {
             const total_cols = rows[i].children[0].getAttribute('colspan');
-            rows[i].children[0].setAttribute('colspan', total_cols + 1);
+            rows[i].children[0].setAttribute('colspan', total_cols + 2);
         } else {
             const project_link = rows[i].getElementsByTagName('a')[0];
             const split_list = project_link.getAttribute('href').split('=');
             const project_id = split_list[split_list.length - 1];
             const project_name = rows[i].children[0].innerText;
 
+            var rate_tag = document.createElement('td');
             var score_tag = document.createElement('td');
-            rows[i].insertBefore(score_tag, rows[i].children[1]);
+            rows[i].insertBefore(rate_tag, rows[i].children[1]);
+            rows[i].insertBefore(score_tag, rows[i].children[2]);
+
             projects[project_name] = {id: project_id, submission: -1, correctness: -1};
-            update_project_stats(project_name, score_tag, total_students, projects);
+            update_project_stats(project_name, rate_tag, score_tag, total_students, projects);
         }
     }
 }
