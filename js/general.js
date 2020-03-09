6cbef232e122f542bbe3c83672fee75f28752427
Kai Ma <k47ma@edu.uwaterloo.ca>
2020-03-08 21:12:08 -0400

    Fixed header links

--- js/general.js
+++ js/general.js
@@ -63,7 +63,19 @@ function add_styles() {
 // add header on scroll
 function add_table_header(table) {
     const rows = table.getElementsByTagName('tr')
-    const header_row = rows[0];
+    const header_rows = new Array();
+    for (const row of rows) {
+        if (row.getElementsByTagName('th')) {
+            header_rows.push(row);
+        } else {
+            break;
+        }
+    }
+    
+    if (header_rows.length == 0) {
+        return;
+    }
+
     var header_copy = document.getElementById('marmostats-overview-header');
     if (!header_copy) {
         header_copy = document.createElement('div');
@@ -72,13 +84,14 @@ function add_table_header(table) {
         header_copy.style.visibility = 'collapse';
     }
 
+    const header_row = header_rows[0];
     header_copy.innerHTML = '';
     header_copy.style.width = header_row.clientWidth + header_row.children.length;
     for (var header of header_row.children) {
         var h_copy = document.createElement('span');
         h_copy.classList.add('marmostats-overview-title');
         var h_text = document.createElement('p');
-        h_text.innerText = header.innerText;
+        h_text.innerHTML = header.innerHTML;
         h_copy.appendChild(h_text);
         header_copy.appendChild(h_copy);
     }
@@ -95,7 +108,7 @@ function add_table_header(table) {
     if (header_rect.top < 0 && header_rect.top + area_height > 0) {
         header_copy.style.position = 'absolute';
         header_copy.style.top = window.pageYOffset - 5;
-        header_copy.style.left = header_rect.left + window.pageXOffset - 1;
+        header_copy.style.left = header_rect.left + window.pageXOffset - 0.5;
         header_copy.style.visibility = 'visible';
     } else {
         header_copy.style.visibility = 'collapse';
@@ -109,6 +122,9 @@ function add_table_header(table) {
     }
     for (var cell_copy of header_copy.children) {
         cell_copy.style.height = max_height;
+        if (cell_copy.innerText == 'Secret') {
+            cell_copy.style.borderLeftWidth = '7px';
+        }
     }
 }
 
